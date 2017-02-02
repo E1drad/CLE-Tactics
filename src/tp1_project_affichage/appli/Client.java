51b108ccef5a4c2178ee8e9aaeca52a69169a275
Bien-CV <baptiste.cv@gmail.com>
2017-02-02 18:17:49 +0100

    U no nothin

--- src/tp1_project_affichage/appli/Client.java
+++ src/tp1_project_affichage/appli/Client.java
@@ -3,25 +3,19 @@ import tp1_project_affichage.framework.*;
 import tp1_project_affichage.thirdParty.*;
 
 public class Client {
-	
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
 		IPersonne p;
 		Object data;
 		IAffichage aff;
 		try{
 			data= BeanLoader.loadBeanWithAge("bean.txt");
-			//IAffichage aff= new AffichageConsole();
-			
-			// new truc pour éviter static
-			 aff= (IAffichage) (new ExtLoader()).loadExtensionv2(IAffichage.class);
+			aff= (IAffichage) (new ExtLoader()).loadExtensionv2(IAffichage.class);
 			aff.affichage((IPersonne)data);
 		}catch(Exception e){
 			System.out.println("Error while loading the data file");
 		}
-		
+
 	}
 
 }
